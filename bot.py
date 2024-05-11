ef8e0ffd81e2e8484715d9ec4f6251f193843fc5
Чжунюй <116472613+DuanZhongyu@users.noreply.github.com>
2024-05-11 19:00:54 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 # Created 2024.04.28
-# by 段仲彧
+# by 彧同学
 
 import telebot
 
-# 彧同学
+# 此处设置了两个机器人，但一个就完全够用
+# 第一个机器人的API
 API_KEY_01 = '您自己的机器人API'
 
-# 最终作品 —— Чатбот ПсихПартнер
+# 第二个机器人的API
 API_KEY_02 = '您自己的机器人API'
 main_bot = telebot.TeleBot(API_KEY_01)
